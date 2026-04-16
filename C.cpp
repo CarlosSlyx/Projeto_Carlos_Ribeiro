67083c376cf7d060470b9ac989c5ed56c92d42f2
CarlosSlyx <cadueduardoslyx@gmail.com>
2026-04-16 15:49:22 -0300

    Criação da Classe C

---
+++ C.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class C {
+private:
+    string C1;
+    int C2;
+
+public:
+    string getC1() {
+        return C1;
+    }
+
+    void setC1(string valor) {
+        C1 = valor;
+    }
+
+    int getC2() {
+        return C2;
+    }
+
+    void setC2(int valor) {
+        C2 = valor;
+    }
+
+    void MC1() {
+        cout << "MC1" << endl;
+    }
+
+    void MC2() {
+        cout << "MC2" << endl;
+    }
+};
